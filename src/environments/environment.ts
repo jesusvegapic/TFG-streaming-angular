5fe2f4ccf32974e39ae3c2f0bdbf82d7ecfd7d1d
Chus <jesusvegapic@gmail.com>
2021-11-30 11:34:39 +0100

    --prod is deprecated --configuration production update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build --configuration production` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 import packageInfo from '../../package.json';
 
