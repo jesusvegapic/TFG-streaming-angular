2ba7a327e72c97d0dca1c5886a4cbeea3baf0a85
Chus <jesusvegapic@gmail.com>
2021-11-30 11:38:26 +0100

    fix enviroment json import error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
-import {name, version} from '../../package.json';
+import packageInfo from '../../package.json';
 
 export const environment = {
   production: true,
-  NAME: name,
-  VERSION: version,
+  NAME: packageInfo.name,
+  VERSION: packageInfo.version,
   REST_USER: 'https://streaming-user.herokuapp.com',
   REST_CORE: 'https://streaming-core.herokuapp.com',
 };
